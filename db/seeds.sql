8ec3103e372f84c970136cac2429b9d835c90216
eric11598 <eric.arellano.0803@gmail.com>
2018-09-19 03:20:18 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Angus Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', true);
+
